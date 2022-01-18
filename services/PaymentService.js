f79cb6c2336ec5fb9ae3615515978ac5c9681597
Facundo Colavini <facu_colavini@hotmail.com>
2022-01-18 11:53:06 -0300

    Payment added to client

---
+++ services/PaymentService.js
@@ -0,0 +1,14 @@
+//Payment service
+class PaymentService {
+    constructor() {
+        this.payment = null;
+    }
+    getPayment() {
+        return this.payment;
+    }
+    setPayment(payment) {
+        this.payment = payment;
+    }
+}
+exports.PaymentService = PaymentService;
+//# sourceMappingURL=PaymentService.js.map
